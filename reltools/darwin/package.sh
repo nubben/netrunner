170a40439fb33548a5b16314fe095615df540cf6
Odilitime <janesmith@airmail.ccl>
2017-07-21 16:08:23 -0700

    tools for packaing OSX releases

---
+++ reltools/darwin/package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+physicalCpuCount=$([[ $(uname) = 'Darwin' ]] &&
+                       sysctl -n hw.physicalcpu_max ||
+                       lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l)
+mkdir netrunner-$(date +%F)
+cd netrunner-$(date +%F)
+cp ../*.dylib .
+cp ../../../*.ttf .
+cd ../../../
+make -j$physicalCpuCount
+cd reltools/darwin/netrunner-$(date +%F)
+cp ../../../netrunner .
+install_name_tool -change /usr/local/lib/libGLEW.2.0.0.dylib libGLEW.2.0.0.dylib netrunner
+install_name_tool -change /usr/local/lib/libfreetype.6.dylib libfreetype.6.dylib netrunner
+install_name_tool -change /usr/local/lib/libharfbuzz.0.dylib libharfbuzz.0.dylib netrunner
+otool -L netrunner
+cd ..
+zip -r -X netrunner-$(date +%F).zip netrunner-$(date +%F)
+rm -fr netrunner-$(date +%F)
+# https://gitgud.io/snippets/29
