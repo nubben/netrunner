7327c30272f188001b7ec2d9985bdbf5dbd57932
Odilitime <janesmith@airmail.cc>
2017-07-26 19:56:15 -0700

    initial some properties (why not?)

--- src/graphics/components/Component.h
+++ src/graphics/components/Component.h
@@ -11,10 +11,10 @@ public:
     bool verticesDirty = false;
     std::shared_ptr<Component> parent = nullptr;
     std::vector<std::shared_ptr<Component>> children;
-    float x;
-    float y;
-    float height;
-    float width;
+    float x=0.0f;
+    float y=0.0f;
+    float height=0.0f;
+    float width=0.0f;
     bool isInline = false;
     std::function<void()> onClick = nullptr;
 };
