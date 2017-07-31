4d6e9da25a8d62ceb34884b65659bf1b8d702259
Odilitime <janesmith@airmail.cc>
2017-07-31 04:21:38 -0700

    save some notes for the future

--- src/graphics/elements/Element.h
+++ src/graphics/elements/Element.h
@@ -10,6 +10,11 @@ class Element {
 public:
     bool isInline = false;
     virtual ~Element();
+    // TODO: remove position
+    // add parent (node or component? well component because that's how the create component is going to calculate position
+    // well right now ComponentBuilder calls renderer and it has parent/window size
+    // component constructors don't really need parent/window size
+    // so we could remove window size too
     virtual std::unique_ptr<Component> renderer(const std::shared_ptr<Node> node, const int x, const int y, const int windowWidth, const int windowHeight);
 };
 
